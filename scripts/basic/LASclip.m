4a4159550169027b63a68836a58e0b1779ab7205
MattParkan <matthew.parkan@gmail.com>
2016-05-23 10:56:39 +0200

    updated header example

--- scripts/basic/LASclip.m
+++ scripts/basic/LASclip.m
@@ -18,7 +18,7 @@ function s = LASclip(points, clipper, varargin)
 %
 % Example:
 %    points = '..\data\measurements\vector\als\6995_2710.las';
-%    outputFilepath = '..\data\measurements\vector\als\6995_2710_subset.las';
+%    outputFilepath = '..\data\measurements\vector\als\zh_6995_2710_subset.las';
 %    xv = [699673; 699729; 699500; 699503];
 %    yv = [271341; 271036; 271042; 271346];
 %    s = LASclip(points, [xv, yv], outputFilepath, 'verbose', true);
