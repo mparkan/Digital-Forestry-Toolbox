c72689d7a35005bbc6dcd3a2312dc6db89d11d49
MattParkan <matthew.parkan@gmail.com>
2017-03-16 17:03:54 +0100

    Updated treeWatershed.m
    Modified marker control watershed algorithm.

--- scripts/tree metrics/treeWatershed.m
+++ scripts/tree metrics/treeWatershed.m
@@ -1,8 +1,8 @@
-function labels = treeWatershed(chm, markers, varargin)
-% TREEWATERSHED - extract Individual Tree Crowns (ITC) labels from a raster Canopy Height Model (CHM) 
-% using marker-controlled watershed segmentation.
+function label = treeWatershed(chm, varargin)
+% TREEWATERSHED - extract Individual Tree Crowns (ITC) label from a raster Canopy Height Model (CHM) 
+% using (optional marker-controlled) watershed segmentation.
 %
-% LABELS = TREEWATERSHED produces the label matrix LABELS containing individual tree crown labels 
+% LABEL = TREEWATERSHED produces the label matrix LABEL containing individual tree crown labels 
 % for each pixel using the approach described in Kwak et al. (2007) [1]. The function takes as input the raster Canopy
 % Height Model CHM and the image coordinates MARKERS of individual tree tops.
 %
@@ -10,12 +10,14 @@ function labels = treeWatershed(chm, markers, varargin)
 % "Detection of individual trees and estimation of tree height using LiDAR data,"
 % J For Res, vol. 12, no. 6, pp. 425–434, Oct. 2007.
 %
-% Syntax:  labels = treeWatershed(chm, markers, ...)
+% Syntax:
+%    label = treeWatershed(chm, ...)
 %
 % Inputs:
 %    chm - RxC numeric matrix, raster Canopy Height Model (CHM) with R rows and C columns
-%
-%    markers - Mx3 numeric matrix, images coordinates (col, row) and height values of tree tops
+%    
+%    markers (optional, default: []) - Mx3 numeric matrix, images coordinates (col, row) and height 
+%    values of markers used in the watershed algorithm
 %
 %    minHeight (optional, default: 1) - numeric value, minimum canopy height, all values 
 %    below this threshold are set to zero.
@@ -25,33 +27,37 @@ function labels = treeWatershed(chm, markers, varargin)
 %    fig (optional, default: true) - boolean value, switch to plot figures
 %
 % Outputs:
-%    labels - RxC numeric matrix, individual tree crown labels
+%    label - RxC numeric matrix, individual tree crown labels
 %
 % Example:
 %    info = geotiffinfo('..\data\measurements\raster\chm\so_2014_woodland_pasture.tif');
 %    [chm, ~] = geotiffread('..\data\measurements\raster\chm\so_2014_woodland_pasture.tif');
 %    
-%    [crh, xyh] = treePeaks(chm, info.RefMatrix, ...
-%        'method', 'fixedRadius', ...
-%        'windowRadius', 3, ...
+%    [crh, xyh] = canopyPeaks(chm, ...
+%        info.RefMatrix, ...
+%        'adjacencyRange', 1.5, ...
+%        'method', 'allometricRadius', ...
+%        'allometry', @(h) 1 + 0.5*log(max(h,1)), ...
 %        'fig', true);
 %
-%    labels = treeWatershed(chm, crh, ...
+%    label = treeWatershed(chm, ...
+%        'markers', crh, ...
 %        'minHeight', 1, ...
-%        'fig', true);
+%        'fig', true, ...
+%        'verbose', true);
 %
 % Other m-files required: none
 % Subfunctions: none
 % MAT-files required: none
-% Compatibility: tested on Matlab R2016a
+% Compatibility: tested on Matlab R2016b
 %
-% See also: treePeaks.m, canopyCover.m
+% See also: canopyPeaks.m, canopyCover.m
 %
 % This code is part of the Matlab Digital Forestry Toolbox
 %
-% Author: Matthew Parkan, EPFL - GIS Research Laboratory
-% Website: http://lasig.epfl.ch/
-% Last revision: May 26, 2016
+% Author: Matthew Parkan, EPFL - GIS Research Laboratory (LASIG)
+% Website: http://mparkan.github.io/Digital-Forestry-Toolbox/
+% Last revision: March 16, 2017
 % Acknowledgments: This work was supported by the Swiss Forestry and Wood Research Fund (WHFF, OFEV), project 2013.18
 % Licence: GNU General Public Licence (GPL), see https://www.gnu.org/licenses/gpl.html for details
 
@@ -61,12 +67,12 @@ function labels = treeWatershed(chm, markers, varargin)
 arg = inputParser;
 
 addRequired(arg, 'chm', @isnumeric);
-addRequired(arg, 'markers', @isnumeric);
-addParamValue(arg, 'minHeight', 1, @(x) isnumeric(x) && (numel(x) == 1));
-addParamValue(arg, 'fig', true, @(x) islogical(x) && (numel(x) == 1));
-addParamValue(arg, 'verbose', true, @(x) islogical(x) && (numel(x) == 1));
+addParameter(arg, 'markers', [], @isnumeric);
+addParameter(arg, 'minHeight', 1, @(x) isnumeric(x) && (numel(x) == 1));
+addParameter(arg, 'fig', true, @(x) islogical(x) && (numel(x) == 1));
+addParameter(arg, 'verbose', true, @(x) islogical(x) && (numel(x) == 1));
 
-parse(arg, chm, markers, varargin{:});
+parse(arg, chm, varargin{:});
 
 
 %% compute gradient magnitude
@@ -109,10 +115,21 @@ if arg.Results.verbose
     
 end
 
-ind_peaks = sub2ind(size(chm), markers(:,2), markers(:,1));
-marker = false(size(chm));
-marker(ind_peaks) = 1;
-labels = watershed(bwdist(marker), 8);
+if isempty(arg.Results.markers) 
+    
+    % basic watershed transform
+    label = watershed(-chm, 8);
+    
+else 
+    
+    % marker controled watershed transform
+    ind_peaks = sub2ind(size(chm), arg.Results.markers(:,2), arg.Results.markers(:,1));
+    marker = false(size(chm));
+    marker(ind_peaks) = 1;
+    I = imimposemin(-chm, marker, 8);
+    label = watershed(I, 8);
+    
+end
 
 if arg.Results.verbose
     
@@ -120,39 +137,35 @@ if arg.Results.verbose
     
 end
 
+%% filter non vegetation pixels
 
-%% match watershed labels with tree top indices
-
-if arg.Results.verbose
-    
-    fprintf('matching watershed labels with marker indices...');
-    
-end
-
-peak_labels = [0; labels(ind_peaks)];
-
-[~,idx] = ismember(labels, 0:size(markers,1));
-labels = peak_labels(idx);
-
-if arg.Results.verbose
-    
-    fprintf('done!\n');
-    
-end
+label(~gradient_mask) = 0;
 
 
-%% filter non forest pixels
-
-labels(~gradient_mask) = 0;
+%% plot results
 
 if arg.Results.fig
     
+    % assign distinct colors to adjacent clusters 
+    [nrows, ncols] = size(label);
+    [xgrid, ygrid] = meshgrid(1:ncols, 1:nrows);
+    buffer = min(10, 3*sqrt(median(accumarray(label(:)+1, label(:), [], @numel))));
+    
+    [color, ~, cmap] = clusterColor([xgrid(:), ygrid(:)], ...
+        label(:), ...
+        'adjacency', '2d', ...
+        'buffer', buffer, ...
+        'colormap', 'cmap12', ...
+        'unlabelledColor', [0.1, 0.1, 0.1], ...
+        'fig', false, ...
+        'verbose', false);
+    
     figure
-    imagesc(labels)
+    imagesc(reshape(color, nrows, ncols))
     axis equal tight
     xlabel('col');
     ylabel('row');
     title('Individual tree crown labels')
+    colormap(cmap);
     
 end
-
