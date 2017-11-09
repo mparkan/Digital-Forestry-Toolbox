0656d88cfb281d64b57ea88406a19ebd07fa6105
MattParkan <matthew.parkan@gmail.com>
2017-11-09 11:17:29 +0100

    Updated canopyCover.m

--- scripts/canopy metrics/canopyCover.m
+++ scripts/canopy metrics/canopyCover.m
@@ -1,11 +1,11 @@
 function cover = canopyCover(chm, refmat, varargin)
 % CANOPYCOVER - computes a canopy crown cover map
 %
-% COVER = CANOPYCOVER(CHM, REFMAT, ...) computes a canopy crown cover map using 2D convolution or a variant of the Delaunay 
-% triangulation based method described in [1]. The cover map is computed from the georeferenced raster canopy height 
+% COVER = CANOPYCOVER(CHM, REFMAT, ...) computes a canopy crown cover map using 2D convolution or a variant of the Delaunay
+% triangulation based method described in [1]. The cover map is computed from the georeferenced raster canopy height
 % model CHM and tree locations STEMS (for the triangulation method).
 %
-% [1] L. Eysn, M. Hollaus, K. Schadauer, et N. Pfeifer, « Forest Delineation Based on Airborne LIDAR Data », 
+% [1] L. Eysn, M. Hollaus, K. Schadauer, et N. Pfeifer, « Forest Delineation Based on Airborne LIDAR Data »,
 % Remote Sensing, vol. 4, p. 762-783, march 2012.
 %
 % Syntax:  cover = canopyCover(chm, refmat, ...)
@@ -29,7 +29,7 @@ function cover = canopyCover(chm, refmat, varargin)
 %    height, all values below this threshold are set to zero.
 %
 %    coverThreshold (optional, default: 0.9) - numeric value between 0 and 1, threshold
-%    of Delaunay triangle filling ratio above which the cover is set to 1  
+%    of Delaunay triangle filling ratio above which the cover is set to 1
 %
 %    filter (optional, default: ones(20,20)) - numeric matrix, convolution window (for convolution method only)
 %
@@ -43,35 +43,41 @@ function cover = canopyCover(chm, refmat, varargin)
 % Example:
 %    info = geotiffinfo('..\data\measurements\raster\chm\so_2014_woodland_pasture.tif');
 %    [chm, ~] = geotiffread('..\data\measurements\raster\chm\so_2014_woodland_pasture.tif');
-%    
-%    [crh, xyh] = treePeaks(chm, info.RefMatrix, ...
-%        'method', 'fixedRadius', ...
-%        'windowRadius', 3, ...
-%        'fig', true);
 %
-%    labels = treeWatershed(chm, crh, ...
+%    [crh, xyh] = canopyPeaks(chm, ...
+%        info.RefMatrix, ...
+%        'method', 'allometricRadius', ...
+%        'allometry', @(h) 1 + 0.5*log(max(h,1)), ...
+%        'adjacency', @(h) min(0.5 + 0.5*log(max(h,1)),4), ...
+%        'fig', true, ...
+%        'verbose', true);
+%
+%    label = treeWatershed(chm, ...
+%        'markers', crh, ...
 %        'minHeight', 1, ...
-%        'fig', true);
+%        'fig', true, ...
+%        'verbose', true);
 %
-%    cover = canopyCover(chm, info.RefMatrix, ...
+%    cover = canopyCover(chm, ...
+%        info.RefMatrix, ...
 %        'method', 'triangulation', ...
 %        'stems', crh, ...
-%        'crowns', labels, ...
+%        'crowns', label, ...
 %        'verbose', true, ...
 %        'fig', true);
 %
 % Other m-files required: none
 % Subfunctions: none
 % MAT-files required: none
-% Compatibility: tested on Matlab R2016a
+% Compatibility: tested on Matlab R2016b
 %
-% See also: treePeaks.m, treeWatershed.m
+% See also: canopyPeaks.m, treeWatershed.m
 %
 % This code is part of the Matlab Digital Forestry Toolbox
 %
-% Author: Matthew Parkan, EPFL - GIS Research Laboratory
-% Website: http://lasig.epfl.ch/
-% Last revision: June 8, 2016
+% Author: Matthew Parkan, EPFL - GIS Research Laboratory (lasig.epfl.ch)
+% Website: http://mparkan.github.io/Digital-Forestry-Toolbox/
+% Last revision: November 11, 2017
 % Acknowledgments: This work was supported by the Swiss Forestry and Wood Research Fund (WHFF, OFEV), project 2013.18
 % Licence: GNU General Public Licence (GPL), see https://www.gnu.org/licenses/gpl.html for details
 
@@ -82,14 +88,14 @@ arg = inputParser;
 
 addRequired(arg, 'chm', @isnumeric);
 addRequired(arg, 'refmat', @isnumeric);
-addParamValue(arg, 'method', 'convolution', @(x) ismember(x, {'convolution', 'triangulation'}));
-addParamValue(arg, 'stems', [], @(x) isnumeric(x) && size(x,1) >= 3);
-addParamValue(arg, 'crowns', [], @(x) isnumeric(x) && isequal(size(x), size(chm)));
-addParamValue(arg, 'minHeight', 1, @(x) isnumeric(x) && (numel(x) == 1));
-addParamValue(arg, 'coverThreshold', 0.9, @(x) isnumeric(x) && (numel(x) == 1) && (x > 0) && (x <= 1));
-addParamValue(arg, 'filter', ones(20,20), @(x) isnumeric(x));
-addParamValue(arg, 'verbose', true, @(x) islogical(x) && (numel(x) == 1));
-addParamValue(arg, 'fig', true, @(x) islogical(x) && (numel(x) == 1));
+addParameter(arg, 'method', 'convolution', @(x) ismember(x, {'convolution', 'triangulation'}));
+addParameter(arg, 'stems', [], @(x) isnumeric(x) && size(x,1) >= 3);
+addParameter(arg, 'crowns', [], @(x) isnumeric(x) && isequal(size(x), size(chm)));
+addParameter(arg, 'minHeight', 1, @(x) isnumeric(x) && (numel(x) == 1));
+addParameter(arg, 'coverThreshold', 0.9, @(x) isnumeric(x) && (numel(x) == 1) && (x > 0) && (x <= 1));
+addParameter(arg, 'filter', ones(20,20), @(x) isnumeric(x));
+addParameter(arg, 'verbose', true, @(x) islogical(x) && (numel(x) == 1));
+addParameter(arg, 'fig', true, @(x) islogical(x) && (numel(x) == 1));
 
 parse(arg, chm, refmat, varargin{:});
 
@@ -142,7 +148,6 @@ switch arg.Results.method
         [nrows, ncols] = size(mask);
         col_stems = double(arg.Results.stems(:,1));
         row_stems = double(arg.Results.stems(:,2));
-        n_trees = length(col_stems);
         
         % compute Delaunay triangulation
         if arg.Results.verbose
@@ -163,7 +168,6 @@ switch arg.Results.method
         [col_grid, row_grid] = meshgrid(1:ncols, 1:nrows);
         row_grid = reshape(row_grid, [], 1);
         col_grid = reshape(col_grid, [], 1);
-        ind_grid = sub2ind([nrows, ncols], row_grid, col_grid);
         
         if arg.Results.verbose
             
@@ -180,57 +184,24 @@ switch arg.Results.method
             
         end
         
-        % compute triangle fill ratio
-        if arg.Results.verbose
-            
-            fprintf('computing canopy / triangle area ratio...');
-            
-        end
-        
-        area_triangle_canopy = accumarray(id_triangulation + 1, reshape(chm > 0, 1, []));
-        area_triangle_total = accumarray(id_triangulation + 1, true(size(id_triangulation)));
-        canopy_fill_ratio = area_triangle_canopy ./ area_triangle_total;
-        
-        % set canopy / triangle area ratio threshold
-        idxl_high_cover = (canopy_fill_ratio >= arg.Results.coverThreshold);
-        
-        if arg.Results.verbose
-            
-            fprintf('done!\n');
-            
-        end
-        
         % compute crown triplet areas
         if ~isempty(arg.Results.crowns)
             
             if arg.Results.verbose
                 
-                fprintf('computing crown areas...');
+                fprintf('computing crown triplet areas...');
                 
             end
             
-            labels_crowns = reshape(arg.Results.crowns,[],1);
-            labels_crowns(sub2ind([nrows, ncols], row_stems, col_stems)) = 1:n_trees; % add marker pixel to label matrix
+            dt_ind = sub2ind([nrows, ncols], dt.Points(:,2), dt.Points(:,1));
+            dt_labels = arg.Results.crowns(dt_ind);
+            dt_labels_triplet = dt_labels(dt.ConnectivityList);
+            idxl_void = any(dt_labels_triplet == 0,2);
+            dt_labels_triplet = dt_labels_triplet(~idxl_void,:);
             
-            area_crowns_chm = accumarray(labels_crowns + 1, reshape(chm > 0, 1, []));
-            area_crowns = accumarray(labels_crowns + 1, true(size(labels_crowns)));
-            
-            % extract crown pixel indices
-            [~, ind_sort] = sort(labels_crowns);
-            col_grid_sort = col_grid(ind_sort);
-            row_grid_sort = row_grid(ind_sort);
-            col_pixel_crowns = mat2cell(col_grid_sort, area_crowns, 1);
-            col_pixel_crowns = col_pixel_crowns(2:end,:);
-            row_pixel_crowns = mat2cell(row_grid_sort, area_crowns, 1);
-            row_pixel_crowns = row_pixel_crowns(2:end,:);
-            
-            % compute crown triplets areas
-            area_crowns_chm = area_crowns_chm(2:end);
-            area_crown_triplets = area_crowns_chm(dt.ConnectivityList);
-            
-            idxl_low_cover = ~idxl_high_cover(2:end);
-            row_crown_triplets = cellfun(@(x1,x2,x3) [x1; x2; x3], row_pixel_crowns(dt.ConnectivityList(idxl_low_cover,1)), row_pixel_crowns(dt.ConnectivityList(idxl_low_cover,2)), row_pixel_crowns(dt.ConnectivityList(idxl_low_cover,3)), 'UniformOutput', 0);
-            col_crown_triplets = cellfun(@(x1,x2,x3) [x1; x2; x3], col_pixel_crowns(dt.ConnectivityList(idxl_low_cover,1)), col_pixel_crowns(dt.ConnectivityList(idxl_low_cover,2)), col_pixel_crowns(dt.ConnectivityList(idxl_low_cover,3)), 'UniformOutput', 0);
+            crown_area = accumarray(arg.Results.crowns(:) + 1, true(size(arg.Results.crowns(:))));
+            crown_area = crown_area(2:end);
+            crown_area_triplet = sum(crown_area(dt_labels_triplet), 2);
             
             if arg.Results.verbose
                 
@@ -241,11 +212,22 @@ switch arg.Results.method
             % compute crown triplets convex hull
             if arg.Results.verbose
                 
-                fprintf('computing crown triplets convex hulls...');
+                fprintf('computing crown triplets convex hull areas...');
                 
             end
             
-            [~, V] = cellfun(@convhull, col_crown_triplets, row_crown_triplets, 'UniformOutput', false); % subset
+            convex_crown_area = zeros(size(crown_area_triplet,1),1);
+            
+            for j = 1:size(crown_area_triplet,1)
+                
+                [row_crown_triplets, col_crown_triplets] = ind2sub([nrows, ncols], find(ismember(arg.Results.crowns, dt_labels_triplet(j,:))));
+                [~, convex_crown_area(j,1)] = convhull(col_crown_triplets, row_crown_triplets);
+                
+            end
+            
+            cover_crown_triplets = -ones(size(dt,1),1);
+            cover_crown_triplets(~idxl_void) = crown_area_triplet ./ convex_crown_area;
+            cover_crown_triplets = [-1; cover_crown_triplets];
             
             if arg.Results.verbose
                 
@@ -253,36 +235,18 @@ switch arg.Results.method
                 
             end
             
-            if arg.Results.verbose
-                
-                fprintf('computing crown cover...');
-                
-            end
-            
-            cover_crown_triplets = zeros(size(area_crowns));
-            cover_crown_triplets(idxl_high_cover(2:end),1) = 1;
-            cover_crown_triplets(idxl_low_cover,1) = sum(area_crown_triplets(idxl_low_cover,:),2) ./ [V{:}]'; % subset
-            
-            cover = zeros(size(chm), 'single');
-            cover(ind_grid(id_triangulation > 0)) = cover_crown_triplets(id_triangulation(id_triangulation > 0));
-            cover(cover > 1) = 1;
+            cover = reshape(cover_crown_triplets(id_triangulation+1), nrows, ncols);
             
             if arg.Results.fig
                 
                 figure
                 imagesc(cover)
-                colormap(gray)
+                colormap(parula)
                 title('Canopy cover ratio')
                 xlabel('col')
                 ylabel('row')
                 axis equal tight
                 colorbar
-                
-            end
-            
-            if arg.Results.verbose
-                
-                fprintf('done!\n');
                 
             end
             
