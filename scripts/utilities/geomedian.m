2262bfd712bcad1a714e353f19d07e9d50dd95e8
MattParkan <matthew.parkan@gmail.com>
2018-04-12 10:31:11 +0200

    Updated geomedian.m
    Updated Octave compatibility

--- scripts/utilities/geomedian.m
+++ scripts/utilities/geomedian.m
@@ -1,27 +1,31 @@
-function [M, I] = geomedian(xyz, varargin)
-% GEOMEDIAN - computes the geometric median of 3D points XYZ
+function [M, I, SSD] = geomedian(X)
+% GEOMEDIAN - computes the geometric median M of points X. Optionally
+% outputs the index I and Sum of Squarred Distances SSD of the geometric
+% median point
 % 
-%
 % Syntax:  [M, I] = geomedian(xyz, ...)
 %
 % Inputs:
-%    xyz - Nx3 numeric matrix, 3D point cloud x,y,z coordinates
+%    X - UxV numeric matrix, point coordinates
 %
 %    fig (optional, default: false) - boolean value, switch to plot figures
 %
 % Outputs:
-%    M - 1x3 numeric vector, geometric median x,y,z coordinates
+%    M - 1xV numeric vector, geometric median coordinates
 %
-%    I - Nx1 numeric vector, index of the points that is the geometric median
+%    I - scalar, index of the geometric median point
+%
+%    SSD - scalar, sum of squarred distances from the the gemoetric median
+%    point to all other points
 %
 % Example:
 %
-%    [M, I] = geomedian(xyz);
+%    [M, I] = geomedian(X);
 %
 % Other m-files required: none
 % Subfunctions: none
 % MAT-files required: none
-% Compatibility: tested on Matlab R2016b
+% Compatibility: tested on Matlab R2017b, GNU Octave 4.2.1 (configured for "x86_64-w64-mingw32")
 %
 % See also:
 %
@@ -29,9 +33,8 @@ function [M, I] = geomedian(xyz, varargin)
 %
 % Author: Matthew Parkan, EPFL - GIS Research Laboratory (LASIG)
 % Website: http://mparkan.github.io/Digital-Forestry-Toolbox/
-% Last revision: February 13, 2017
-% Acknowledgments: This work was supported by the Swiss Forestry and Wood
-% Research Fund, WHFF (OFEV) - project 2013.18
+% Last revision: April 12, 2018
+% Acknowledgments: This work was supported by the Swiss Forestry and Wood Research Fund, WHFF (OFEV) - project 2013.18
 % Licence: GNU General Public Licence (GPL), see https://www.gnu.org/licenses/gpl.html for details
 
 
@@ -39,59 +42,34 @@ function [M, I] = geomedian(xyz, varargin)
 
 arg = inputParser;
 
-addRequired(arg, 'xyz', @(x) (size(x,2) == 3) && isnumeric(x));
-addParameter(arg, 'fig', false, @(x) islogical(x) && (numel(x) == 1));
+addRequired(arg, 'X', @(x) isnumeric(x));
 
-parse(arg, xyz, varargin{:});
+parse(arg, X);
 
 
 %% compute pairwise squared distances between all points
 
-if size(xyz,1) >= 2
+if size(X,1) >= 2
     
     % find unique combinations of all point pairs
-    pairs = nchoosek(1:size(xyz, 1), 2);
+    pairs = nchoosek(1:size(X, 1), 2);
     
     % compute pairwise squared distances between all unique points
-    dist = sum((xyz(pairs(:, 1),:) - xyz(pairs(:, 2),:)) .^ 2, 2);
+    dist = sum((X(pairs(:, 1),:) - X(pairs(:, 2),:)) .^ 2, 2);
     PAIRS = [pairs; pairs(:,2:-1:1)];
     DIST = [dist; dist];
     
-    % compute sum of squarred distances for each point
-    SDS = accumarray(PAIRS(:,1), DIST, [], @(x) sum(x));
+    % compute the sum of squarred distances for each point
+    SSD = accumarray(PAIRS(:,1), DIST, [], @(x) sum(x));
     
-    % find point that minmizes sum of squarred distances to all others
-    [SDS_min, I] = min(SDS);
-    M = xyz(I,:);
-    
-    if arg.Results.fig
-        
-        figure
-        scatter3(xyz(:,1), ...
-            xyz(:,2), ...
-            xyz(:,3), ...
-            6, ...
-            [0,0,0], ...
-            'Marker', '.')
-        hold on
-        scatter3(xyz(I,1), ...
-            xyz(I,2), ...
-            xyz(I,3), ...
-            12, ...
-            [1,0,0], ...
-            'Marker', 'o')
-        axis equal tight vis3d
-        xlabel('x')
-        ylabel('y')
-        zlabel('z')
-        title('before majority vote')
-        
-    end
+    % find the point that minimizes the sum of squarred distances to all others
+    [SSD, I] = min(SSD);
+    M = X(I,:);
     
 else
     
     I = 1;
-    M = xyz;
+    M = X;
     
 end
 
