28b69ddbac416da592e855de43950d1da8a453b0
Matt Parkan <matthew.parkan@gmail.com>
2018-09-20 16:34:48 +0200

    Updated elevationModels.m

--- scripts/grids/elevationModels.m
+++ scripts/grids/elevationModels.m
@@ -2,9 +2,9 @@ function [models, refmat] = elevationModels(xyz, classification, varargin)
 % ELEVATIONMODELS - computes a Digital Terrain Model (DTM), a Digital Surface Model
 % (DSM), a Digital Height Model (DHM) and point density maps from the 3D classified points defined by XYZ and CLASSIFICATION.
 %
-% [MODELS, REFMAT] = ELEVATIONMODELS(XYZ, CLASSIFICATION, ...) returns a structure containing 
-% elevation model values (MODELS.TERRAIN.VALUES, MODELS.SURFACE.VALUES, MODELS.HEIGHT.VALUES) and a boolean mask with 
-% missing data areas (MODELS.MASK) and point density maps (MODELS.DENSITY.OVERALL, MODELS.DENSITY.TERRAIN, MODELS.DENSITY.SURFACE) 
+% [MODELS, REFMAT] = ELEVATIONMODELS(XYZ, CLASSIFICATION, ...) returns a structure containing
+% elevation model values (MODELS.TERRAIN.VALUES, MODELS.SURFACE.VALUES, MODELS.HEIGHT.VALUES) and a boolean mask with
+% missing data areas (MODELS.MASK) and point density maps (MODELS.DENSITY.OVERALL, MODELS.DENSITY.TERRAIN, MODELS.DENSITY.SURFACE)
 % from the classified 3D point cloud defined by XYZ and CLASSIFICATION.
 %
 % Syntax:  [models, refmat] = elevationModels(xyz, classification, ...)
@@ -14,22 +14,30 @@ function [models, refmat] = elevationModels(xyz, classification, varargin)
 %
 %    classification - Nx1 numeric vector, containing point classification
 %
-%    cellSize (optional, default: 1) - numeric value, pixel (grid cell) size (in the same units as XY). If only cellSize is provided, 
+%    cellSize (optional, default: 1) - numeric value, pixel (grid cell) size (in the same units as XY). If only cellSize is provided,
 %    the upper left xy corner point is used as the center of the first
 %    pixel. The cellSize argument is ignored if refmat is provided.
 %
-%    refmat (optional, default: []) - 3x2 numeric matrix, spatial referencing matrix, such that xy_map = [row, col, ones(nrows,1)] * refmat. 
+%    refmat (optional, default: []) - 3x2 numeric matrix, spatial referencing matrix, such that xy_map = [row, col, ones(nrows,1)] * refmat.
 %    The cellSize argument is ignored if refmat is provided.
 %
 %    rasterSize (optional, default: []) - 1x2 numeric vector, target size [nrows, ncols] of the
-%    output raster. Points that fall outside the extent of the raster are ignored. 
+%    output raster. Points that fall outside the extent of the raster are ignored.
 %
 %    classTerrain (optional, default: [2, 9]) - numeric vector, point classes used to represent the terrain
 %
 %    classSurface (optional, default: [3, 4, 5, 6]) - numeric vector, point
 %    classes used to represent the surface (note that points with terrain class are also used)
 %
-%    closing (optional, default: 1) - numeric value, the maximum distance from data mask at which missing values (holes) are filled (i.e
+%    interpolation (optional, default: 'idw') - interpolation method, 'linear', 'nearest', 'natural', 'idw'
+%
+%    searchRadius (optional, default: inf) -, inf, numeric value, search radius (in map units) used with the inverse distance 
+%    weighting interpolation method
+%    
+%    weightFunction (optional, default: @(d) d^-3, @(x)) - anonymous function handle of the form @(d) = ..., 
+%    weight function (in map units) used with the inverse distance weighting interpolation method
+%
+%    closing (optional, default: 1) - numeric value, the maximum distance (in map units) from data mask at which missing values (holes) are filled (i.e
 %    interpolated/extrapolated). Use inf to fill all missing values.
 %
 %    smoothingFilter (optional, default: []) - numeric matrix, two-dimensional smoothing
@@ -54,6 +62,7 @@ function [models, refmat] = elevationModels(xyz, classification, varargin)
 %        'classTerrain', [2], ...
 %        'classSurface', [4,5], ...
 %        'cellSize', 1, ...
+%        'interpolation', 'idw', ...
 %        'closing', 5, ...
 %        'smoothingFilter', fspecial('gaussian', [3, 3], 0.5), ...
 %        'outputModels', {'terrain', 'surface', 'height', 'density'}, ...
@@ -70,7 +79,7 @@ function [models, refmat] = elevationModels(xyz, classification, varargin)
 % Other m-files required: rasterize.m
 % Subfunctions: none
 % MAT-files required: none
-% Compatibility: tested on Matlab R2017b, GNU Octave 4.2.2 (configured for "x86_64-w64-mingw32")
+% Compatibility: tested on Matlab R2017b, GNU Octave 4.4.1 (configured for "x86_64-w64-mingw32")
 %
 % See also: rasterize.m
 %
@@ -78,7 +87,7 @@ function [models, refmat] = elevationModels(xyz, classification, varargin)
 %
 % Author: Matthew Parkan, EPFL - GIS Research Laboratory (LASIG)
 % Website: http://mparkan.github.io/Digital-Forestry-Toolbox/
-% Last revision: April 20, 2018
+% Last revision: September 20, 2018
 % Acknowledgments: This work was supported by the Swiss Forestry and Wood Research Fund (WHFF, OFEV), project 2013.18
 % Licence: GNU General Public Licence (GPL), see https://www.gnu.org/licenses/gpl.html for details
 
@@ -96,6 +105,10 @@ addParameter(arg, 'refmat', [], @(x) (all(size(x) == [3, 2]) && isnumeric(x)) ||
 addParameter(arg, 'rasterSize', [], @(x) (isnumeric(x) && (numel(x) == 2) && all(x > 0)) || isempty(x));
 addParameter(arg, 'closing', 1, @(x) isnumeric(x) & (numel(x) == 1) & (x >= 0));
 addParameter(arg, 'smoothingFilter', [], @isnumeric);
+addParameter(arg, 'interpolation', 'linear', @(x) ismember(x, {'nearest', 'natural', 'linear', 'idw'}));
+addParameter(arg, 'searchRadius', inf, @(x) isnumeric(x) & (numel(x) == 1) & (x >= 1));
+addParameter(arg, 'weightExponent', 3, @(x) isnumeric(x) & (numel(x) == 1) & (x >= 1));
+addParameter(arg, 'weightFunction', @(d) d^-3, @(x) strfind(func2str(x),'@(d)') == 1);
 addParameter(arg, 'outputModels', {'terrain', 'surface', 'height', 'density'}, @(x) iscell(x) & any(ismember(x, {'terrain', 'surface', 'height', 'density'})));
 addParameter(arg, 'fig', true, @(x) islogical(x) && (numel(x) == 1));
 addParameter(arg, 'verbose', true, @(x) islogical(x) && (numel(x) == 1));
@@ -110,11 +123,11 @@ if isempty(arg.Results.refmat)
     dx = arg.Results.cellSize;
     dy = -arg.Results.cellSize;
     refmat = [0, dy; dx, 0; min(xyz(:,1))-dx, max(xyz(:,2))-dy];
-
+    
 else
     
     refmat = arg.Results.refmat;
-    dx = refmat(2,1); 
+    dx = refmat(2,1);
     dy = refmat(1,2);
     
 end
@@ -159,7 +172,7 @@ end
 if arg.Results.closing > 0
     
     [D, ~] = bwdist(mask);
-    mask = D <= ceil(arg.Results.closing / dx);
+    mask = D <= ceil(arg.Results.closing / abs(dx));
     
 end
 
@@ -233,29 +246,93 @@ if any(ismember(arg.Results.outputModels, {'terrain', 'surface', 'height'}))
     
     if any(idxl_terrain)
         
-        if arg.Results.verbose
-            
-            fprintf('computing terrain model...');
-            
-        end
-        
+
         %[nrows, ncols] = size(mask);
         [col_grid, row_grid] = meshgrid(1:ncols, 1:nrows);
         t = [row_grid(:), col_grid(:)] * refmat(1:2,:);
         x_grid = reshape(t(:,1) + refmat(3,1), nrows, ncols);
         y_grid = reshape(t(:,2) + refmat(3,2), nrows, ncols);
         
-        % interpolation
-        dtm = griddata(xyz(idxl_terrain,1), ...
-            xyz(idxl_terrain,2), ...
-            xyz(idxl_terrain,3), ...
-            x_grid, ...
-            y_grid, ...
-            'linear');
+        OCTAVE_FLAG = (exist('OCTAVE_VERSION', 'builtin') ~= 0); % determine if system is Matlab or GNU Octave
         
-        % extrapolation (nearest neighbour only)
-        [~, idxn_nn] = bwdist(~isnan(dtm));
-        dtm = dtm(idxn_nn);
+        if OCTAVE_FLAG && ~strcmp(arg.Results.interpolation, 'idw')
+            
+            interpolation = 'idw';
+            fprintf('\nWarning: Octave only supports inverse distance weighting interpolation\n');
+            % Octave depends on the QHull library to compute the Delaunay triangulation required for
+            % linear, nearest and natural neighbour interpolation. In its current version QHull downsamples 
+            % the triangulation when the Qz option is specified (see https://github.com/qhull/qhull/issues/25) 
+            % and is too slow for most practical purposes when the Qz option is disabled.
+            
+        else
+            
+            interpolation = arg.Results.interpolation;
+            
+        end
+        
+        if arg.Results.verbose
+            
+            fprintf('computing terrain model with %s interpolation...', interpolation);
+            
+        end
+
+        switch interpolation
+            
+            case {'nearest', 'natural', 'linear'}
+                
+                % interpolation
+                F = scatteredInterpolant(xyz(idxl_terrain,1), ...
+                    xyz(idxl_terrain,2), ...
+                    xyz(idxl_terrain,3), ...
+                    interpolation, ...
+                    'linear');
+                
+                dtm = F(x_grid, y_grid);
+                
+            case {'idw'}
+                
+                [dtm, ~] = rasterize(xyz(idxl_terrain,1:2), ...
+                    xyz(idxl_terrain,3), ...
+                    'refmat', refmat, ...
+                    'rasterSize', [nrows, ncols], ...
+                    'fun', @(x) sum(x)/numel(x), ...
+                    'fill', nan, ...
+                    'fig', false);
+                
+                % compute search radius in pixels 
+                if isinf(arg.Results.searchRadius)
+                    
+                    r = ceil(max(max(bwdist(~isnan(dtm))))); % radius to fill all holes
+                    
+                else
+                    
+                    r = ceil(arg.Results.searchRadius ./ abs(dx));
+                    
+                end
+                
+                % create structuring element for convolution
+                SE = double(bwdist(padarray(true, double([r,r]))));
+                SE(r+1,r+1) = 0.001; % central element of convolution window
+                SE(SE > r) = 0;
+                
+                % apply weight function
+                SE = arrayfun(arg.Results.weightFunction, SE * abs(dx));
+                SE(isinf(SE)) = 0;
+                
+                idxl_nan = isnan(dtm);
+                dtm_i = dtm;
+                dtm_i(idxl_nan) = 0;
+                
+                % sum of values
+                V = imfilter(dtm_i, SE);
+                
+                % sum of weights
+                W = imfilter(double(~idxl_nan), SE);
+                
+                % normalize
+                dtm = V ./ W;
+                
+        end
         
         % smooth terrain model values
         if ~isempty(arg.Results.smoothingFilter)
@@ -274,7 +351,7 @@ if any(ismember(arg.Results.outputModels, {'terrain', 'surface', 'height'}))
         
         % store values and interpolant in structure
         models.terrain.values = dtm;
-
+        
         % display terrain model plot
         if arg.Results.fig
             
@@ -287,7 +364,7 @@ if any(ismember(arg.Results.outputModels, {'terrain', 'surface', 'height'}))
             colorbar
             
         end
-        
+
         % compute terrain model point density
         if any(ismember(arg.Results.outputModels, {'density'}))
             
@@ -298,7 +375,7 @@ if any(ismember(arg.Results.outputModels, {'terrain', 'surface', 'height'}))
                 'fun', @numel, ...
                 'fill', 0, ...
                 'fig', false);
-    
+            
             models.density.terrain = single(density_terrain);
             models.density.terrain(~models.mask) = nan;
             
@@ -364,7 +441,7 @@ if any(ismember(arg.Results.outputModels, {'surface', 'height'}))
         fprintf('\nWarning: no smoothing filter was applied to surface model\n');
         
     end
-        
+    
     dsm(~models.mask) = nan;
     models.surface.values = single(dsm);
     
